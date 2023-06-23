e8398da1d5b21c6a1ed0ef92070a2462959ae84e
mushfik <mushfiqr116@gmail.com>
2023-06-23 08:06:26 +0600

    pages done

--- ecom/src/pages/CartPage.jsx
+++ ecom/src/pages/CartPage.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import Cart from '../features/cart/Cart'
+import Navbar from '../features/navbar/Navbar'
 
 const CartPage = () => {
   return (
-    <div><Cart/></div>
+    
+    <div>
+
+<Navbar>
+
+<Cart/>
+</Navbar>
+</div>
   )
 }
 
